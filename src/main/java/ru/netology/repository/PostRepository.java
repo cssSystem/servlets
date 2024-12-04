5b9d51c2b6815ac29b5f67ff4c6df4108bcb7a00
system <system-slike@yandex.ru>
2024-12-04 22:10:09 +0300

    Initial commit

---
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,42 @@
+package ru.netology.repository;
+
+import ru.netology.model.Post;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.atomic.AtomicLong;
+
+// Stub
+public class PostRepository {
+    private final ConcurrentMap<Long, Post> posts;
+    private final AtomicLong counter;
+
+    public PostRepository() {
+        this.posts = new ConcurrentHashMap<>();
+        this.counter = new AtomicLong();
+    }
+
+    public Collection<Post> all() {
+        return posts.values();
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(posts.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            post.setId(counter.incrementAndGet());
+        }
+        posts.put(post.getId(), post);
+        return post;
+    }
+
+    public void removeById(long id) {
+        posts.remove(id);
+    }
+}
